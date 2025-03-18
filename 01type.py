7dfcac749bb5c612ad616b2480f095c38aeb2486
310-0 <leo@shingu.ac.kr>
2025-03-18 12:18:24 +0900

    리스트공부

--- 01type.py
+++ 01type.py
@@ -43,6 +43,15 @@ print(f'{tc=} ===> {tf=}')
 print(2 ** 3 , 2 ** (1/2), 2 ** (-1/2))
 
 # 사칙연산중에서 0으로 나누는 것은 허용하지 않음
-print(3/0)
+#print(3/0)
 
+a = "True"
+print("변수 a의 자료형은", type(a))
 
+a='3'
+b = float(a)
+print(b**int(a))
+
+a='3.5'
+b = 4
+print(a * b)
---
+++ 02list.py
@@ -0,0 +1,52 @@
+# 2025.3.18
+# 파이썬 리스트 : 한개의 변수에 여러 값을 할당
+from idlelib.help_about import version
+
+# 인덱싱
+colors = ['red', 'blue', 'green']
+print(colors)  # 리스트 전체 출력
+print(colors[1]) # 리스트 두번째 원소 출력, 0부터 시작
+print(colors[-1]) # 리스트 마지막 원소 출력
+print(len(colors)) # 리스트 길이 출력
+
+# 슬라이싱
+cities = ['서울', '부산', '인천', '의정부', '대전', '강릉', '논산', '포항']
+print(cities[0:5])  # cities[0:n]  0~ n-1까지 표시
+print(cities[:7]) # 0~6번째 원소
+print(cities[:-1]) # 0 ~ -2 까지
+print(cities[3:])
+print(cities[:])  # 전부 표시
+print(cities[-4:])  # 뒤에서부터 4번째부터 표시
+print(cities[:7:2]) # 처음부터 6번째 원소까지, 한간씩 건너뜀
+print(cities[::3]) # 처음부터 끝까지, 세칸씩 건너뜀
+print(cities[::-1]) # 처음부터 끝까지, 거꾸로
+print(cities[4::-2])
+
+# 리스트의 추가
+colors = ['red', 'blue', 'green']
+colors.append('white')
+print(colors[:])
+colors.extend(['black', 'purple'])
+print(colors[:])
+colors.insert(1, 'orange')
+print(colors[:])
+colors.remove('purple')
+print(colors[:])
+colors[1] = 'sky'
+print(colors[:])
+
+# 패킹, 언패킹
+c1, _, c2, c3, _, _ = colors
+print(c1, c2, c3)
+
+# 퀴즈
+first = ["egg", "salad", "bread", "soup", "coffee"]
+second = ["fish", "lamb", "pork", "beef", "chicken"]
+third = ["apple", "ban" "오렌지", "포도", "망고"]
+
+order = [first, second, third]
+john = [order[0][-2],  second[1::3], third[0]]
+print(john)
+del john[2]
+john.extend(([order[2][0:1]]))
+print(john)
